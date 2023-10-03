fab341362f8b46d7783bc5b1a1e57c80bef59e13
Kjell Larsson <kjell.m.larsson@se.ibm.com>
2023-10-03 16:59:22 +0200

    added rest api

---
+++ datetimeserviceJava/com/ibm/clientengineering/restsubflowimplementation/GetDatetime_JavaCompute.java
@@ -0,0 +1,60 @@
+package com.ibm.clientengineering.restsubflowimplementation;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.ibm.broker.javacompute.MbJavaComputeNode;
+import com.ibm.broker.plugin.MbElement;
+import com.ibm.broker.plugin.MbException;
+import com.ibm.broker.plugin.MbJSON;
+import com.ibm.broker.plugin.MbMessage;
+import com.ibm.broker.plugin.MbMessageAssembly;
+import com.ibm.broker.plugin.MbOutputTerminal;
+import com.ibm.broker.plugin.MbUserException;
+
+public class GetDatetime_JavaCompute extends MbJavaComputeNode {
+
+	public void evaluate(MbMessageAssembly inAssembly) throws MbException {
+
+		MbOutputTerminal out = getOutputTerminal("out");
+
+		MbMessage outMessage = new MbMessage();
+		MbMessageAssembly outAssembly = new MbMessageAssembly(inAssembly, outMessage);
+
+		try {
+
+			MbElement rootElement = outMessage.getRootElement();
+
+			MbElement outJsonRoot = rootElement.createElementAsLastChild(MbJSON.PARSER_NAME);
+
+			MbElement outJsonData = outJsonRoot.createElementAsLastChild(MbElement.TYPE_NAME, MbJSON.DATA_ELEMENT_NAME,
+					null);
+			
+			
+			Date now = new Date();
+			
+			DateFormat timeFormat = new SimpleDateFormat("h:mm:ss a");
+			DateFormat dateFormat = new SimpleDateFormat("EEE, MMM d, yyyy");
+			
+			
+			addJsonChildToElement(outJsonData, "date", dateFormat.format(now));
+			addJsonChildToElement(outJsonData, "time", timeFormat.format(now));
+			
+
+		} catch (MbException e) {
+			throw e;
+		} catch (RuntimeException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new MbUserException(this, "evaluate()", "", "", e.toString(), null);
+		}
+		out.propagate(outAssembly);
+	}
+
+	private void addJsonChildToElement(MbElement element, String name, String value) throws MbException {
+		MbElement child = element.createElementAsLastChild(MbElement.TYPE_NAME_VALUE);
+		child.setName(name);
+		child.setValue(value);
+	}
+}
